df0b1e2d6532253a1c5f855667612a237f25ce8b
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-02-07 17:27:58 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -2,8 +2,6 @@ class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
 
   def change
     add_column :students, :grade, :integer
-    add_column :students, :birthdate, :string 
-  end 
-end 
-
-
+    add_column :students, :birthdate, :string
+  end
+end
